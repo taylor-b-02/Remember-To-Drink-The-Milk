0d0266363bde4d833984693110fcb225479795ba
Taylor Barnabic <tbarnabic1@gmail.com>
2021-08-23 19:47:25 -0400

    Defined more user-related routes

--- db/models/user.js
+++ db/models/user.js
@@ -1,27 +1,31 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email:{
-      type: DataTypes.STRING(150),
-      allowNull: false,
-      unique: true
-    } ,
-    userName: {
-      allowNull: false,
-      type: Sequelize.STRING(50),
-      unique: true
-    },
-    dateOfBirth: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    hashedPassword: {
-      allowNull: false,
-      type: Sequelize.STRING.BINARY
-    },
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+	const User = sequelize.define(
+		"User",
+		{
+			email: {
+				type: DataTypes.STRING(150),
+				allowNull: false,
+				unique: true,
+			},
+			userName: {
+				allowNull: false,
+				type: DataTypes.STRING(50),
+				unique: true,
+			},
+			dateOfBirth: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+			hashedPassword: {
+				allowNull: false,
+				type: DataTypes.STRING.BINARY,
+			},
+		},
+		{}
+	);
+	User.associate = function (models) {
+		// associations can be defined here
+	};
+	return User;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,185 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/models');
-const bcrypt = require('bcryptjs');
-const { csrfProtection, asyncHandler, handleValidationErrors } = require('../utils');
+const db = require("../db/models");
+const bcrypt = require("bcryptjs");
+const {
+	csrfProtection,
+	asyncHandler,
+	handleValidationErrors,
+} = require("../utils");
 
-const { check } = require('express-validator');
+const { check } = require("express-validator");
 
 const { User } = db;
 
+const loginValidator = [
+	check("login-identifier")
+		.exists({ checkFalsy: true })
+		.withMessage(
+			"Please provide your Username or Email Address for authentication"
+		),
+	check("password")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a value for Password"),
+];
+
+const userValidators = [
+	check("username")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a value for Last Name")
+		.isLength({ max: 50 })
+		.withMessage("Last Name must not be more than 50 characters long"),
+	check("email")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a value for Email Address")
+		.isLength({ max: 150 })
+		.withMessage("Email Address must not be more than 150 characters long")
+		.isEmail()
+		.withMessage("Email Address is not a valid email")
+		.custom((value) => {
+			return db.User.findOne({ where: { emailAddress: value } }).then(
+				(user) => {
+					if (user) {
+						return Promise.reject(
+							"The provided Email Address is already in use by another account"
+						);
+					}
+				}
+			);
+		}),
+	check("password")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a value for Password")
+		.isLength({ max: 50 })
+		.withMessage("Password must not be more than 50 characters long")
+		.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+		.withMessage(
+			'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+		),
+	check("confirmPassword")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a value for Confirm Password")
+		.isLength({ max: 50 })
+		.withMessage(
+			"Confirm Password must not be more than 50 characters long"
+		),
+	check("dateOfBirth")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide your date of birth"),
+];
 
 /* GET users listing. */
 //GET user profile
-router.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
-    const userId = parseInt(req.params.id, 10);
-    const user = await db.User.findByPk(userId);
-    res.render('user-profile', { title: 'User', user });
-
-}));
+router.get(
+	"/:id(\\d+)",
+	asyncHandler(async (req, res, next) => {
+		const userId = parseInt(req.params.id, 10);
+		const user = await db.User.findByPk(userId);
+		res.render("user-profile", { title: "User", user });
+	})
+);
 
 //GET registration page with form
-router.get('/register', csrfProtection, asyncHandler(async(req, res) => {
-    res.render('user-register', { title: 'Register' })
-}));
+router.get(
+	"/register",
+	csrfProtection,
+	asyncHandler(async (req, res) => {
+		res.render("user-register", { title: "Register" });
+	})
+);
 
 //GET login page with form
-router.get('/login', csrfProtection, asyncHandler(async(req, res) => {
-    res.render('user-login', {
-        title: "Login",
-        csrfToken: req.csrfToken()
-    })
-}));
+router.get(
+	"/login",
+	csrfProtection,
+	asyncHandler(async (req, res) => {
+		res.render("user-login", {
+			title: "Login",
+			csrfToken: req.csrfToken(),
+		});
+	})
+);
 
-//POST to login page (log in) NEEDS user/password validator
-router.post('/login', csrfProtection, handleValidationErrors, asyncHandler(async(req, res) => {
+//POST to login page (log in)
+router.post(
+	"/login",
+	csrfProtection,
+	loginValidator,
+	handleValidationErrors,
+	asyncHandler(async (req, res) => {
+		const { loginIdentifier, password } = req.body;
 
-}))
+		let errors = [];
+		const validatorErrors = validationResult(req);
+		if (user) {
+			if (loginIdentifier.includes("@")) {
+				const user = await User.findOne({
+					where: {
+						email: loginIdentifier,
+					},
+				});
+			} else {
+				const user = await user.findOne({
+					where: {
+						username: loginIdentifier,
+					},
+				});
+			}
+
+			if (validatorErrors.isEmpty()) {
+				// const match = await bcrypt.compare(password, user.hashedPassword);
+				const passwordMatch = await bcrypt.compare(
+					password,
+					user.hashedPassword.toString()
+				);
+
+				if (passwordMatch) {
+					loginUser(req, res, user);
+					return res.redirect("/lists");
+				}
+				errors.push(
+					"Login failed for the provided email address/username and password"
+				);
+			} else {
+				errors = validatorErrors.array().map((error) => error.msg);
+			}
+		}
+
+		res.render("user-login", {
+			title: "Login",
+			loginIdentifier,
+			errors,
+			csrfToken: req.csrfToken(),
+		});
+	})
+);
 
 //GET login page with form
-router.get('/logout', csrfProtection, asyncHandler(async(req, res) => {
+router.get(
+	"/logout",
+	csrfProtection,
+	asyncHandler(async (req, res) => {})
+);
 
-}));
+//POST new users (register)
+router.post(
+	"/",
+	csrfProtection,
+	userValidators,
+	handleValidationErrors,
+	asyncHandler(async (req, res) => {
+		const { username, email, password, dateOfBirth } = req.body;
 
-//POST new users (register) NEEDS form validation
-router.post('/', csrfProtection, handleValidationErrors, asyncHandler(async(req, res) => {
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-}))
-
-
+		await User.create({
+			username: username,
+			email: email,
+			hashedPassword: hashedPassword,
+			dateOfBirth: dateOfBirth,
+		});
+		res.redirect("/lists");
+	})
+);
 
 module.exports = router;
