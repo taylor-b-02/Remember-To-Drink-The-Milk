916d27d07f0645fbf2c9500d8f266fd34dcf1dcb
sherryyu <bambooyu715@gmail.com>
2021-08-23 16:41:32 -0400

    table-model-generated

---
+++ db/migrations/20210823203704-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(150),
+        unique: true
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      dateOfBirth: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
