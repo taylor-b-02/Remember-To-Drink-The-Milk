4552f1d7d9a6c3ed938484f57cb78b74b72a0b68
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-08-23 17:11:26 -0400

    finished migration and model
    user-model, user migration

--- db/models/user.js
+++ db/models/user.js
@@ -1,10 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    userName: DataTypes.STRING,
-    dateOfBirth: DataTypes.DATE,
-    hashedPassword: DataTypes.STRING
+    email:{
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      unique: true
+    } ,
+    userName: {
+      allowNull: false,
+      type: Sequelize.STRING(50),
+      unique: true
+    },
+    dateOfBirth: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    hashedPassword: {
+      allowNull: false,
+      type: Sequelize.STRING.BINARY
+    },
   }, {});
   User.associate = function(models) {
     // associations can be defined here
