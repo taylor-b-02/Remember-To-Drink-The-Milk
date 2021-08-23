6d313edd9b4190b16028905ffdae1db9d788b76d
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-08-23 16:46:15 -0500

    Working on user routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db/models');
+const bcrypt = require('bcryptjs');
+
+const { check } = require('express-validator');
+
+const { User } = db;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', (req, res) => {
+    res.render()
+})
+
+router.post('/', (req, res) => {
+
+})
 
 router
 
