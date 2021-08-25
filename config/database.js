aef6d153584f7c472c48afe6248bf0ceacc2b6fc
Taylor Barnabic <tbarnabic1@gmail.com>
2021-08-25 18:55:53 -0400

    Tweaked url setup for DB (Heroku)

--- config/database.js
+++ config/database.js
@@ -11,8 +11,7 @@ module.exports = {
 		dialect: "postgres",
 	},
 	production: {
-		use_env_variable:
-			"postgres://hxunyzivyxladn:f45b3efc67a9cc6be9be4291e6e5c76c9822fe42e6bb06ec2d3994cefc15823a@ec2-44-196-132-15.compute-1.amazonaws.com:5432/dcqvr2r0akrgq7",
+		use_env_variable: "DATABASE_URL",
 		dialect: "postgres",
 		seederStorage: "sequelize",
 	},
