87a47f1fd11de9dfee36a4163418315896493c70
Taylor Barnabic <tbarnabic1@gmail.com>
2021-08-25 18:52:46 -0400

    Changing db_url

--- config/database.js
+++ config/database.js
@@ -11,7 +11,8 @@ module.exports = {
 		dialect: "postgres",
 	},
 	production: {
-		use_env_variable: db_url,
+		use_env_variable:
+			"postgres://hxunyzivyxladn:f45b3efc67a9cc6be9be4291e6e5c76c9822fe42e6bb06ec2d3994cefc15823a@ec2-44-196-132-15.compute-1.amazonaws.com:5432/dcqvr2r0akrgq7",
 		dialect: "postgres",
 		seederStorage: "sequelize",
 	},
