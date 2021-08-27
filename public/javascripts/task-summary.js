16b4c91de13f121d4cb1c1a4aa3cf3a7b876c2f0
sherryyu <bambooyu715@gmail.com>
2021-08-27 17:29:44 -0400

    tasks-summary-functionality-achieved

--- public/javascripts/task-summary.js
+++ public/javascripts/task-summary.js
@@ -1,34 +1,65 @@
 window.addEventListener("DOMContentLoaded", event => {
 
     // Everytime a user clicks on Add Task button, tasks summary will increment by 1.
-    const addTaskBtn = document.querySelector("#add-task-btn");
-    let TaskSum = document.querySelector("#tasks-sum");
-    let TaskSumInt = parseInt(TaskSum.innerHTML);
+    const addTaskBtn = document.getElementById("add-task-btn");
+    let taskSum = document.getElementById("tasks-sum");
+    let taskSumInt = parseInt(localStorage.getItem("tasksRecord"));
 
-    addTaskBtn.addEventListener("click", (event) => {
-        TaskSumInt += 1;
-        TaskSum.innerHTML = TaskSumInt;
+    if(!taskSumInt) {
+        taskSumInt = 0;
+        localStorage.setItem("tasksRecord", taskSumInt);
+    }
+
+    addTaskBtn.addEventListener("click", event => {
+        taskSumInt += 1;
+        localStorage.setItem("tasksRecord", taskSumInt);
+        taskSum.innerHTML = taskSumInt;
     });
 
-    // Everytime a user clicks on the Delete button, tasks summary will decrement by 1.
-    const deleteTaskBtn = document.querySelector(".task-btn.red-btn");
-
-    deleteTaskBtn.addEventListener("click", (event) => {
-        TaskSumInt -= 1;
-        TaskSum.innerHTML = TaskSumInt;
+    //Everytime a user clicks on the Delete button, tasks summary will decrement by 1.
+    const deleteTaskBtnArr = document.querySelectorAll(".task-btn.red-btn");
+    // console.log(deleteTaskBtn);
+    // console.log(typeof(taskSumInt));
+    deleteTaskBtnArr.forEach(deleteBtn => {
+        deleteBtn.addEventListener("click", event => {
+            if(taskSumInt > 0) {
+                taskSumInt -= 1;
+                // console.log(taskSumInt);
+                localStorage.setItem("tasksRecord", taskSumInt);
+                taskSum.innerHTML = taskSumInt;
+            }
+        })
     })
+
+    let tasksRecord = localStorage.getItem("tasksRecord");
+    if(tasksRecord) {
+        taskSum.innerHTML = tasksRecord;
+    }
+
+
 
     // When a user clicks on the checkbox, completed increment by 1.
     // acquire the checkbox by its id
     // acquire completed calculator by its id, then key into innerHTML
-    const completionCheckbox = document.querySelector("#completion-checkbox");
+    const completionCheckbox = document.querySelectorAll("#completion-checkbox");
     let completionSum = document.querySelector("#completed-sum");
-    console.log(`Is there a sum: ${completionSum}`);
-    let completionSumInt = parseInt(completionSum.innerHTML);
+    let completionSumInt = parseInt(localStorage.getItem("completed-tasks"));
 
-    completionCheckbox.addEventListener("click", (event) => {
-        completionSumInt += 1;
-        completionSum.innerHTML = completionSumInt;
+    if(!completionSumInt) {
+        completionSumInt = 0;
+        localStorage.setItem("completed-tasks", completionSumInt);
+    }
+
+    completionCheckbox.forEach(checkbox => {
+        checkbox.addEventListener("click", event => {
+            completionSumInt += 1;
+            localStorage.setItem("completed-tasks", completionSumInt);
+            completionSum.innerHTML = completionSumInt;
+        })
     })
 
+    let completedTasksRecord = localStorage.getItem("completed-tasks");
+    if(completedTasksRecord) {
+        completionSum.innerHTML = completedTasksRecord;
+    }
 });
