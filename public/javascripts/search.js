baf419c61ef8c1729ee1f33d7ceed5ca918a7997
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-08-27 14:56:19 -0500

    Working on search function

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,7 +1,15 @@
+const db = require("../db/models");
+const { User, Task } = db;
 
-function searchFunction() {
+async function searchFunction() {
     const input = document.getElementById("nav-search-input");
     const inputValue = input.value;
-    console.log(inputValue);
+    const userId = parseInt(req.params.id, 10);
+    const tasks = await db.Task.findAll({
+        where: {
+            userId: userId,
+            description: { $like: inputValue}
+        },
+    });
 }
 document.getElementById("nav-search-input").addEventListener('search', searchFunction);
