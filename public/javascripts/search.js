cceea6eac87dd84790afcef36e5e5f64d0aa997b
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-08-27 13:06:27 -0500

    Fixed search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,7 +1,7 @@
-document.getElementById("nav-search-input").addEventListener('search', searchFunction);
 
 function searchFunction() {
     const input = document.getElementById("nav-search-input");
     const inputValue = input.value;
     console.log(inputValue);
 }
+document.getElementById("nav-search-input").addEventListener('search', searchFunction);
