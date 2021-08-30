52856c6073c3629379cf8f161a8eda01a6f2c1aa
Taylor Barnabic <tbarnabic1@gmail.com>
2021-08-30 07:50:11 -0400

    Altered seeder files

--- db/seeders/20210828181606-Lists.js
+++ db/seeders/20210828181606-Lists.js
@@ -1,46 +1,53 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
+	up: (queryInterface, Sequelize) => {
+		/*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       */
-   return queryInterface.bulkInsert('Lists', [
-       {
-        id: 99,
-        name:'Today',
-        ownerId: 99999,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-       {
-        id: 100,
-        name:'Tomorrow',
-        ownerId: 99999,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-       {
-        id: 101,
-        name:'Next Week',
-        ownerId: 99999,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-   ], {});
+		return queryInterface.bulkInsert(
+			"Lists",
+			[
+				{
+					id: 99,
+					name: "Today",
+					ownerId: 99999,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 100,
+					name: "Tomorrow",
+					ownerId: 99999,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 101,
+					name: "Next Week",
+					ownerId: 99999,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
 
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
+	down: (queryInterface, Sequelize) => {
+		/*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       */
-   return queryInterface.bulkDelete('Lists', null, {});
-  }
+		return queryInterface.bulkDelete("Lists", null, {
+			truncate: true,
+			cascade: true,
+			restartIdentity: true,
+		});
+	},
 };
--- db/seeders/20210828230517-Tasks.js
+++ db/seeders/20210828230517-Tasks.js
@@ -1,79 +1,86 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
+	up: (queryInterface, Sequelize) => {
+		/*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       */
-   return queryInterface.bulkInsert('Tasks', [
-       {
-           id: 999,
-           isComplete: false,
-           description: 'Drink 3 glasses of water',
-           userId: 99999,
-           listId: 99,
-           createdAt: new Date(),
-           updatedAt: new Date()
-       },
-       {
-        id: 1000,
-        isComplete: false,
-        description: 'Meditate before work',
-        userId: 99999,
-        listId: 99,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-       {
-        id: 1001,
-        isComplete: false,
-        description: 'Go jogging',
-        userId: 99999,
-        listId: 100,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-       {
-        id: 1002,
-        isComplete: false,
-        description: 'Do grocery shopping',
-        userId: 99999,
-        listId: 100,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-       {
-        id: 1003,
-        isComplete: false,
-        description: 'Go to therapy appointment',
-        userId: 99999,
-        listId: 101,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
-       {
-        id: 1004,
-        isComplete: false,
-        description: 'Eat 3 servings of vegetables',
-        userId: 99999,
-        listId: 101,
-        createdAt: new Date(),
-        updatedAt: new Date()
-       },
+		return queryInterface.bulkInsert(
+			"Tasks",
+			[
+				{
+					id: 999,
+					isComplete: false,
+					description: "Drink 3 glasses of water",
+					userId: 99999,
+					listId: 99,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 1000,
+					isComplete: false,
+					description: "Meditate before work",
+					userId: 99999,
+					listId: 99,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 1001,
+					isComplete: false,
+					description: "Go jogging",
+					userId: 99999,
+					listId: 100,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 1002,
+					isComplete: false,
+					description: "Do grocery shopping",
+					userId: 99999,
+					listId: 100,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 1003,
+					isComplete: false,
+					description: "Go to therapy appointment",
+					userId: 99999,
+					listId: 101,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					id: 1004,
+					isComplete: false,
+					description: "Eat 3 servings of vegetables",
+					userId: 99999,
+					listId: 101,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
 
-   ], {});
-  },
-
-  down: (queryInterface, Sequelize) => {
-    /*
+	down: (queryInterface, Sequelize) => {
+		/*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       */
-   return queryInterface.bulkDelete('Tasks', null, {});
-  }
+		return queryInterface.bulkDelete("Tasks", null, {
+			truncate: true,
+			cascade: true,
+			restartIdentity: true,
+		});
+	},
 };
