9c6d08f3efe76c4cb291e5726ec3d00beb22d431
Taylor Barnabic <tbarnabic1@gmail.com>
2021-08-30 07:53:10 -0400

    Removed debugging log

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get("env") === "development" ? err : {};
-	console.log("ERROR SJFL:SKEJDF::", res.local.message);
 	// render the error page
 	res.status(err.status || 500);
 	res.render("error");
